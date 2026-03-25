35dcdb6858f349dd0d8eb77f035cbc34e8c2e06e
edwin <edwinjoseavilorio@gmail.com>
2026-03-24 22:52:34 -0500

    se realiza la primera vercion esperimental del codigo antes de entragar la principal

--- auth.js
+++ auth.js
@@ -3,9 +3,8 @@
 // ========================================
 
 // Función para cerrar sesión
-// Elimina la sesión del usuario y lo redirige al login
 function logout() {
-  localStorage.removeItem('loggedIn');  // Elimina la autenticación guardada
-  window.location.replace('./components/login/login.html');  // Redirige al login
+  localStorage.removeItem('loggedIn');
+  window.location.replace('./components/login/login.html');
 }
 
